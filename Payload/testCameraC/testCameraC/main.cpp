b786bf65c7a5f0c6079f5c87de3d30b2e7c5181f
Megan Cvitan <53129937+megancvitan@users.noreply.github.com>
2020-07-13 14:00:17 -0400

    No need for hardware trigger mode anymore

--- Payload/testCameraC/testCameraC/main.cpp
+++ Payload/testCameraC/testCameraC/main.cpp
@@ -23,41 +23,14 @@ int main()
 	{
 		return 0;
 	}
+	
 	MSG msg;
 
 	//Selecting which mode to run the camera with 
 	int nMode;
-	if (verbose)
-	{
-		cout << "Please Select mode:" << endl;
-		cout << "Press S for Sotware mode" << endl;
-		cout << "Press H for Hardware mode" << endl;
-	}
-	// Awaits input from user (Either hardware or software trigger input)
-	while (1)
-	{
-		if (_kbhit())
-		{
-			//If the User presses S, the mode will be switched to software trigger mode 
-			int key = _getch();
-			if ((key == 's') || (key == 'S'))
-			{
-				nMode = IS_SET_TRIGGER_SOFTWARE;
-				break;
-			}
+	nMode = IS_SET_TRIGGER_SOFTWARE;
 
-			//If the User presses H, the mode will be switched to hardware trigger mode (rising edge)
-			if ((key == 'h') || (key == 'H'))
-			{
-				nMode = IS_SET_TRIGGER_LO_HI;
-				break;
-			}
-		}
-		PeekMessage(&msg, NULL, 0, 0, PM_REMOVE);
-		DispatchMessage(&msg);
-	}
-	//Sets the trigger mode.
-	//TO DO: need to do something special if it is hardware triggered!
+	//Sets the trigger mode
 	nRet = is_SetExternalTrigger(hCam, nMode);
 	if (nRet != IS_SUCCESS)
 	{
@@ -86,7 +59,7 @@ int main()
 		{
 			if (verbose)
 			{
-				cout << "Whoops, something went wrong" << endl;
+				cout << "Something went wrong" << endl;
 			}
 			return 1;
 		}
