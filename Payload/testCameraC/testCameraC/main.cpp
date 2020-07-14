d75dd9233def6fd34cb7d4d0462a2b7e5b7f6729
Youssef <joebestavros@live.com>
2020-07-13 22:10:40 -0400

    Finalized the captureBurst function and commented the code a bit

--- Payload/testCameraC/testCameraC/main.cpp
+++ Payload/testCameraC/testCameraC/main.cpp
@@ -10,8 +10,14 @@
 
 using namespace std;
 
-int main()
+int main(int args, char *argv[])
 {
+	wchar_t path[100] = _T("C:\\Users\\joebe\\Desktop\\image.jpg");
+	//The input to the main function is going to be: 1) path folder to save the image in. 
+	//2) mode number (0 being capture a single frame, 1 being capture a burst of frames, 2 being capture a video)
+	//3) burst size
+	
+	//To troubleshoot, code would print out some statements
 	BOOL verbose = verb();
 	int nRet = 0;
 
@@ -39,30 +45,27 @@ int main()
 			cout << "Camera has failed to switch to trigger mode" << endl << endl;
 			cout << "Terminating Program..." << endl;
 		}
-		return 1;
+		return 0;
+	}
+	if (verbose) 
+	{
+			cout << "Set the camera to trigger mode" << endl;
+	}
+	
+	int mode = (int) argv[1];
+	if (mode == 0)
+	{
+		nRet = captureSingle(hCam, path);
+	}
+	else if (mode==1)
+	{
+		nRet = captureBurst(hCam, path, (int)argv[2]);
+
 	}
 	else
 	{
-		if (verbose) 
-		{
-			cout << "Set the camera to trigger mode" << endl;
-		}
-		nRet = captureSingle(hCam);
-		if (nRet == 1)
-		{
-			if (verbose) 
-			{
-				cout << "Program ran successfully" << endl;
-			}
-		}
-		else
-		{
-			if (verbose)
-			{
-				cout << "Something went wrong" << endl;
-			}
-			return 1;
-		}
+		nRet = captureVideo(hCam,path); //NOT FINISHED YET 
 	}
-	return 0;
+	
+	return 1;
 }
