449afc6c1e4d28b32cf97ba7534220591d94a8a6
Joe1best <joebestavros@live.com>
2020-07-24 23:53:08 +0000

    commented the bash script

--- Payload_Linux/run.sh
+++ Payload_Linux/run.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+#Checks for the main executable file just in case it is not there
+#If it isnt, run the make command that will run the Makefile
 if [[ ! -f "main" ]]
 then 
 	`make`
 fi
 
+#Here we run what tha make command outputs and give the c code two inputs:
+#		$1: which mode the camera is operating in (right now either 
+#		0 for singleCapture and 1 for BurstCapture)
+#		#2: how many pictures to take in a burst
 ./main $1 $2
+
+#does a cleanup of the folder after this script has been executed
 rm -r main
 rm -r main.o
 rm -r cameraLibrary.o
