96ff6fa5df4a0793cedcaeed26e6b2996ec6c450
Megan Cvitan <53129937+megancvitan@users.noreply.github.com>
2020-07-29 21:57:38 -0400

    Trying something new

---
+++ Payload_Linux/stm32_I2C.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <wiringPiI2C.h>
+
+// define the address of the device
+#define DEVICE_ID 0x53
+
+// define the registers
+#define REG_POWER_CTL   0x2D
+#define REG_DATA_X_LOW  0x32
+#define REG_DATA_X_HIGH 0x33
+#define REG_DATA_Y_LOW  0x34
+#define REG_DATA_Y_HIGH 0x35
+#define REG_DATA_Z_LOW  0x36
+#define REG_DATA_Z_HIGH 0x37
+
+int main (int argc, char *argv){
+	// setting up the I2C communication
+	int fd = wiringPiI2CSetup(DEVICE_ID);
+
+	// if the set up does not work, give message
+	if fd == (-1){
+		std::cout << "Failed to init I2C communication. \n";
+		return -1;
+	}
+	std::cout << "I2C communication successful. \n";
+
+	// writing to a register
+	// the 4th bit of an 8 bit register
+	wiringPiI2CWriteReg8(fd, REG_POWER_CTL, 0b00001000);
+}
