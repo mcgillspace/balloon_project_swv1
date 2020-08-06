65d7606a902534b809a97dcd9e6289740676476e
Joebest123 <joebestavros@live.com>
2020-08-05 21:56:35 -0400

    Update run_I2C.py

--- Payload_Linux/run_I2C.py
+++ Payload_Linux/run_I2C.py
@@ -7,8 +7,11 @@ addr = 0x8
 while (True): 
 
 	order = SMBus(1).read_byte(addr)
-	print (order)
-	if order==1:
-		os.system("./run.sh 1 30")
-		 
-	
+	if (order==0):
+		os.system("./run.sh 1 15")
+	elif (order==1):
+		os.system("./run.sh 2 15")
+	elif (order==2):
+		os.system("./run.sh 3 15")
+	elif (order==9):
+		os.system("reboot")
