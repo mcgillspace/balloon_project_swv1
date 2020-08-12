cabc3d332df3ccbc31c1b96fdb4baa6f0c079bd0
Joe1best <joebestavros@live.com>
2020-08-12 16:00:29 -0400

    Took some sensor data with delay

--- Payload_Linux/Sensor_Analysis/uncertaintyBME280.py
+++ Payload_Linux/Sensor_Analysis/uncertaintyBME280.py
@@ -15,18 +15,18 @@ colours = cm.rainbow(np.linspace(0, 1, 4))
 fig , [ax0,ax1,ax2,ax3]= plt.subplots(4,1, figsize=(22,20))
 
 ax0.set_ylabel("Humidity (%)",color=colours[0],fontsize=22)
-ax0.plot(humidity)
+ax0.plot(humidity,'.')
 ax0.xaxis.set_visible(False)
 
 ax1.set_ylabel("Pressure (hPa)",color=colours[1],fontsize=22)
-ax1.plot(pressure)
+ax1.plot(pressure,'.')
 ax1.xaxis.set_visible(False)
 
 ax2.set_ylabel("Temperature (C)",color=colours[2],fontsize=22)
-ax2.plot(temperature)
+ax2.plot(temperature,'.')
 ax2.xaxis.set_visible(False)
 
 ax3.set_ylabel("Altitude (m)",color=colours[3],fontsize=22)
-ax3.plot(altitude)
+ax3.plot(altitude,'.')
 
 #plt.show()
--- Payload_Linux/bme280_sensor.py
+++ Payload_Linux/bme280_sensor.py
@@ -5,7 +5,7 @@ import adafruit_bme280
 from time import gmtime, strftime
 
 NUMTRIALS = 3000
-TIME_BETWEEN_TAKES = 0
+TIME_BETWEEN_TAKES = 10
 
 def takeData(bme280):
 	# Units: 0 - 100%, hPa, m, celsius
@@ -29,7 +29,7 @@ bme280.sea_level_pressure = 1013.25
 
 #Creates a file called adaSensor if it does not already exist 
 #and appends data to it 
-file = open("/home/alarm/Desktop/balloon_project_swv1/Payload_Linux/Sensor_Analysis/adaSensor.txt", "a")
+file = open("/home/alarm/Desktop/balloon_project_swv1/Payload_Linux/Sensor_Analysis/adaSensor_delay.txt", "a")
 
 for i in range(NUMTRIALS):
 	data = takeData(bme280)
