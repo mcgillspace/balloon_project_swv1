81f75e5534174fed6e3aaacd334e0814e9a7d9db
Megan Cvitan <megan.cvitan@mail.mcgill.ca>
2020-08-11 21:32:13 -0400

    Update bme280_sensor.py

--- Payload_Linux/bme280_sensor.py
+++ Payload_Linux/bme280_sensor.py
@@ -1,7 +1,7 @@
-import time 
 import board
 import busio
 import adafruit_bme280
+from time import gmtime, strftime
 
 # Create library object using our Bus I2C port
 i2c = busio.I2C(board.SCL, board.SDA)
@@ -21,9 +21,12 @@ MINUTES_BETWEEN_READS = 5
 humidity = format(bme280.humidity, ".1f")
 pressure = format(bme280.pressure, ".1f")
 temp_c = bme280.temperature
+time_taken = strftime("%d %b %Y %H:%M:%S", gmtime())
 
 # saving sensor data to file found in raspberry pi
-with open("/home/alarm/Desktop/adaSensor.csv", 'w') as file:
-        file.write(temp_c, pressure, humidity) 
+# creating file if not already made; if made, write to it
+f = open("/home/alarm/Desktop/adaSensor.csv", "a+")
+f.write(time_taken, temp_c, pressure, humidity) 
+f.close()
         
               
