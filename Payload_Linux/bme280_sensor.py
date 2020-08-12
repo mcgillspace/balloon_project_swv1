857cec7e698f1bf4c36f42075de5a881e8fdfd0e
Megan Cvitan <megan.cvitan@mail.mcgill.ca>
2020-08-11 21:01:49 -0400

    Took off streamer and temperature in fahrenheit

--- Payload_Linux/bme280_sensor.py
+++ Payload_Linux/bme280_sensor.py
@@ -2,28 +2,23 @@ import time
 import board
 import busio
 import adafruit_bme280
-from ISStreamer.Streamer import Streamer
 
 # Create library object using our Bus I2C port
 i2c = busio.I2C(board.SCL, board.SDA)
 bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c)
  
-# --------- User Settings ---------
+# User settings
 SENSOR_LOCATION_NAME = "ANSERIS-I"
-BUCKET_NAME = ":partly_sunny: Room Temperature"
-BUCKET_KEY = "temp1"
-ACCESS_KEY = "YOUR ACCESS KEY HERE"
+
 # change this to match the launch location's pressure (hPa) at sea level
 bme280.sea_level_pressure = 1013.25
 MINUTES_BETWEEN_READS = 5
-# ---------------------------------
 
 # OR create library object using our Bus SPI port
 #spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
 #bme_cs = digitalio.DigitalInOut(board.D10)
 #bme280 = adafruit_bme280.Adafruit_BME280_SPI(spi, bme_cs)
 
-streamer = Streamer(bucket_name=BUCKET_NAME, bucket_key=BUCKET_KEY, access_key=ACCESS_KEY)
 while: True
         humidity = format(bme280.humidity, ".1f")
         pressure = format(bme280.pressure, ".1f")
