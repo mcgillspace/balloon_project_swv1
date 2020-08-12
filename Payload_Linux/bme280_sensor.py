fb12b69471c4945a560f60631928e059f9c4d18f
Joe1best <joebestavros@live.com>
2020-08-11 23:19:33 -0400

    Starting to analyze the statistical error related to the sensor.
    Co-authored-by: Megan Cvitan <megan.cvitan@mail.mcgill.ca>

--- Payload_Linux/bme280_sensor.py
+++ Payload_Linux/bme280_sensor.py
@@ -3,25 +3,36 @@ import busio
 import adafruit_bme280
 from time import gmtime, strftime
 
+NUMTRIALS = 1000
+
+
+def takeData(bme280):
+	# Units: 0 - 100%, hPa, m, celsius
+	humidity = format(bme280.humidity, ".2f")
+	pressure = format(bme280.pressure, ".2f")
+	altitude = format(bme280.altitude, ".2f")
+	temp_c = format(bme280.temperature,".2f")
+	time_taken = strftime("%d %b %Y %H:%M:%S", gmtime())
+	data = time_taken + "\t" + f"{humidity}" + "\t" + f"{pressure}"+ "\t"+ f"{temp_c}"+"\t"+ f"{altitude}"+"\n"
+
+	return data	
+
 # Create library object using our Bus I2C port
 i2c = busio.I2C(board.SCL, board.SDA)
-bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c)
+
+bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c, address = 0x77)
 
 # change this to match the launch location's pressure (hPa) at sea level
 bme280.sea_level_pressure = 1013.25
 
-# Units: 0 - 100%, hPa, m, celsius
-humidity = format(bme280.humidity, ".1f")
-pressure = format(bme280.pressure, ".1f")
-altitude = format(bme280.altitude, ".1f")
-temp_c = bme280.temperature
-time_taken = strftime("%d %b %Y %H:%M:%S", gmtime())
 
-# Saving sensor data to file found in raspberry pi
-# Creating file if not already made; if made, write to it
-data = time_taken + "\t" + f"{humidity}" + "\t" + f"{pressure}"+ "\t"+ f"{temp_c}"+"\t"+ f"{altitude}"+"\n"
+#Creates a file called adaSensor if it does not already exist 
+#and appends data to it 
 file = open("/home/alarm/Desktop/adaSensor.txt", "a")
-file.write(line)
 
-# Closes file
-file.close()         
+for i in range(NUMTRIALS):
+	data = takeData(bme280)
+	file.write(data)
+
+file.close()
+         
