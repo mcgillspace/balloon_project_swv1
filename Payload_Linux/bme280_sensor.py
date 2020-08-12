53a6c5e191fb33c2783ef980923bc6143f9c24bd
Joebest123 <joebestavros@live.com>
2020-08-11 22:10:06 -0400

    Update bme280_sensor.py

--- Payload_Linux/bme280_sensor.py
+++ Payload_Linux/bme280_sensor.py
@@ -9,30 +9,18 @@ bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c)
 
 # change this to match the launch location's pressure (hPa) at sea level
 bme280.sea_level_pressure = 1013.25
-MINUTES_BETWEEN_READS = 5
 
-# OR create library object using our Bus SPI port
-#spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
-#bme_cs = digitalio.DigitalInOut(board.D10)
-#bme280 = adafruit_bme280.Adafruit_BME280_SPI(spi, bme_cs)
-
-
-# units: hPa, celsius
+# Units: hPa, celsius
 humidity = format(bme280.humidity, ".1f")
 pressure = format(bme280.pressure, ".1f")
 temp_c = bme280.temperature
 time_taken = strftime("%d %b %Y %H:%M:%S", gmtime())
 
-# saving sensor data to file found in raspberry pi
-# creating file if not already made; if made, write to it
-data = [time_taken, f"{humidity}", f"{pressure}", f"{temp_c}"]
+# Saving sensor data to file found in raspberry pi
+# Creating file if not already made; if made, write to it
+data = time_taken + "\t" + f"{humidity}" + "\t" + f"{pressure}"+ "\t"+ f"{temp_c}"+"\n"
 file = open("adaSensor.txt", "a")
+file.write(line)
 
-for line in data:
-  # write line to output file
-  file.write(line)
-  file.write("\n")
-  
-file.close()
-        
-              
+# Closes file
+file.close()         
